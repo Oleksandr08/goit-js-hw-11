e731d7c271578672d0e1a0b5cd2c368e48675a00
Oleksandr08 <is4uk2011@icloud.com>
2025-07-21 22:41:17 +0300

    Update render-functions.js

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,32 +1,43 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 let lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
+  captionsData: 'alt',
+  captionDelay: 250,
 });
-  
+
 export function renderImages(images, gallery) {
-    const markup = images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-      return `
-        <li class="gallery-item">
-          <a href="${largeImageURL}" class="gallery-link">
-            <img src="${webformatURL}" alt="${tags}" class="gallery-image" />
-          </a>
-          <div class="image-info">
-            <div class="image-info-item"><span><b>Likes</b></span><span>${likes}</span></div>
-            <div class="image-info-item"><span><b>Views</b></span><span>${views}</span></div>
-            <div class="image-info-item"><span><b>Comments</b></span><span>${comments}</span></div>
-            <div class="image-info-item"><span><b>Downloads</b></span><span>${downloads}</span></div>
-          </div>
-        </li>
-      `;
-    }).join('');
-  
-    gallery.insertAdjacentHTML('beforeend', markup);
-    lightbox.refresh();
-  }
-  
-  export function clearGallery(gallery) {
-    gallery.innerHTML = '';
-  }
+  const markup = images
+    .map(
+      ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+        return `
+      <li class="gallery-item">
+        <a href="${largeImageURL}" class="gallery-link">
+          <img src="${webformatURL}" alt="${tags}" class="gallery-image" />
+        </a>
+        <div class="image-info">
+          <div><b>Likes:</b> ${likes}</div>
+          <div><b>Views:</b> ${views}</div>
+          <div><b>Comments:</b> ${comments}</div>
+          <div><b>Downloads:</b> ${downloads}</div>
+        </div>
+      </li>`;
+      }
+    )
+    .join('');
+
+  gallery.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery(gallery) {
+  gallery.innerHTML = '';
+}
+
+export function showLoader() {
+  document.querySelector('.loader-wrapper').classList.remove('is-hidden');
+}
+
+export function hideLoader() {
+  document.querySelector('.loader-wrapper').classList.add('is-hidden');
+}
